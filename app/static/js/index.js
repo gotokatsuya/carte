d0578107da5c4e854e1d64b71806d05c3b9db07f
Shuhei Katori <shuhei.katori@eure.jp>
2017-04-20 00:40:46 +0900

    Set circle to follow the marker

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,18 +1,21 @@
 $(document).ready(function () {
 
   var center = new google.maps.LatLng(35.681298, 139.766247);
-  
+
   var mapOptions = {
     zoom: 12,
     center: center
   };
 
   var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-      
+
   var marker = new google.maps.Marker({
     position: map.getCenter(),
     draggable: true,
     map: map
+  });
+  marker.addListener("drag", function() {
+    circle.setCenter(marker.getPosition());
   });
 
   var geocoder = new google.maps.Geocoder();
@@ -27,19 +30,19 @@ $(document).ready(function () {
     center: center,
     radius: 3000
   });
-  
+
   $("#search").click(function(){
-    
+
     var address = $("#address").val();
-    
+
     geocoder.geocode({ 'address': address }, function (results, status) {
       if (status === google.maps.GeocoderStatus.OK) {
-      
+
         var loc = results[0].geometry.location;
         map.setCenter(loc);
         marker.setPosition(loc);
         circle.setCenter(loc);
-      
+
         $("#latitude").val(loc.lat());
         $("#longitude").val(loc.lng());
       }
