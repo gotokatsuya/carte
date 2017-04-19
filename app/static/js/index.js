b847ff563dae5612d76a5da7a27691d9bf653ee2
gotokatsuya <peach921110@gmail.com>
2017-04-20 00:01:36 +0900

    Delete geo

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,90 +1,32 @@
 $(document).ready(function () {
 
-  var map;
-  var marker;
-  var geocoder;
-  var circle;
- 
-  if(navigator.geolocation){
+  var center = new google.maps.LatLng(35.681298, 139.766247);
+  
+  var mapOptions = {
+    zoom: 12,
+    center: center
+  };
 
-    navigator.geolocation.getCurrentPosition(
-    function( position )
-    {
-      var data = position.coords;
-
-      var lat = data.latitude;
-      var lng = data.longitude;
-
-      $("#latitude").val(lat);
-      $("#longitude").val(lng);
-        
-      var center = new google.maps.LatLng(lat,lng);
-
-      var mapOptions = {
-        zoom: 12,
-        center: center
-      };
-
-      map = new google.maps.Map(document.getElementById('map'), mapOptions);
-      marker = new google.maps.Marker({
-        position: map.getCenter(),
-        draggable: true,
-        map: map
-      });
-
-      geocoder = new google.maps.Geocoder();
-
-      circle = new google.maps.Circle({
-        strokeColor: '#FF0000',
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: '#FF0000',
-        fillOpacity: 0.35,
-        map: map,
-        center: center,
-        radius: 3000
-      });
-
-    },
-    function( err )
-    {
-      console.log(err);
-
-      var center = new google.maps.LatLng(35.681298,139.766247);
-
-      var mapOptions = {
-        zoom: 12,
-        center: center
-      };
-
-      map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
       
-      marker = new google.maps.Marker({
-        position: map.getCenter(),
-        draggable: true,
-        map: map
-      });
+  var marker = new google.maps.Marker({
+    position: map.getCenter(),
+    draggable: true,
+    map: map
+  });
 
-      geocoder = new google.maps.Geocoder();
+  var geocoder = new google.maps.Geocoder();
 
-      circle = new google.maps.Circle({
-        strokeColor: '#FF0000',
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: '#FF0000',
-        fillOpacity: 0.35,
-        map: map,
-        center: center,
-        radius: 3000
-      });
-    },
-    {
-      "enableHighAccuracy": false,
-      "timeout": 6000,
-      "maximumAge": 180000,
-    });
-  }
-
+  var circle = new google.maps.Circle({
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: '#FF0000',
+    fillOpacity: 0.35,
+    map: map,
+    center: center,
+    radius: 3000
+  });
   
   $("#geocode").click(function(){
     
